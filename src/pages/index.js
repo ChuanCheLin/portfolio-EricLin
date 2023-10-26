9952054f8cb2ea3c1f0f8d207916cf94b6ffdc8e
ChuanCheChe <diamond16888@gmail.com>
2023-10-26 00:36:54 -0500

    hardcoded the size for SunIcon, MoonIcon, and LinkArrow to fix weird rendering problem

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
                   rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
                   border-2 border-solid border-transparent hover:border-dark
                   dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
-                  hover:dark:border-light md:p-2 md:px-4 md:text-base xs:p-1 xs:px-2"
+                  hover:dark:border-light md:p-2 md:px-4 md:text-base"
                 >
                   Resume <LinkArrow className={"w-6 ml-1"} />
                 </Link>
